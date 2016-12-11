8f5373e314f7080fc42e893891aefb17f6a401e0
Petraea <g.j.s.dawes@gmail.com>
2016-12-11 20:28:03 +0100

    Add Connect dialog.

--- wx-simple.py
+++ wx-simple.py
@@ -21,9 +21,64 @@ class InfoPane(wx.ListCtrl):
         self.SetColumnWidth(0, 220)
         self.SetColumnWidth(1, 70)
 
+class ConnectWindow(wx.Dialog):
+    def __init__(self, parent, id):
+        wx.Dialog.__init__(self, parent, -1, 'Connect to a Server')
+
+        pnl = wx.Panel(self)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
+        sb = wx.StaticBox(pnl, label='Connection Settings')
+        sbs = wx.StaticBoxSizer(sb, orient=wx.VERTICAL)
+
+        ldapuri = wx.BoxSizer(wx.HORIZONTAL)
+        ldapuri.Add(wx.StaticText(pnl, label='LDAP URI'))
+        ldapuri.Add(wx.TextCtrl(pnl), flag=wx.LEFT, border=5)
+        sbs.Add(ldapuri)
+
+        binddn = wx.BoxSizer(wx.HORIZONTAL)
+        binddn.Add(wx.StaticText(pnl, label='Bind DN'))
+        binddn.Add(wx.TextCtrl(pnl), flag=wx.LEFT, border=5)
+        sbs.Add(binddn)
+
+        passwd = wx.BoxSizer(wx.HORIZONTAL)
+        passwd.Add(wx.StaticText(pnl, label='Password'))
+        passwd.Add(wx.TextCtrl(pnl,style=wx.TE_PASSWORD), flag=wx.LEFT, border=5)
+        sbs.Add(passwd)
+
+        sasl = wx.CheckBox(pnl,label='Use SASL')
+        sasl.SetValue(False)
+        sasl.Disable()
+        sbs.Add(sasl)
+
+        sbs.Add(wx.RadioBox(pnl,label='Protocol',choices=['Automatic','Force 2','Force 3'],
+                majorDimension = 1,style = wx.RA_SPECIFY_COLS))
+        pnl.SetSizer(sbs)
+
+        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        okButton = wx.Button(self, label='Ok')
+        closeButton = wx.Button(self, label='Close')
+        hbox2.Add(okButton)
+        hbox2.Add(closeButton, flag=wx.LEFT, border=5)
+
+        vbox.Add(pnl, proportion=1,
+            flag=wx.ALL|wx.EXPAND, border=5)
+        vbox.Add(hbox2,
+            flag=wx.ALIGN_CENTER|wx.TOP|wx.BOTTOM, border=10)
+
+        self.SetSizer(vbox)
+        self.Fit()
+        okButton.Bind(wx.EVT_BUTTON, self.OnClose)
+        closeButton.Bind(wx.EVT_BUTTON, self.OnClose)
+
+        self.ShowModal()
+
+    def OnClose(self, e):
+        self.Destroy()
+
 class LdapBrowser(wx.Frame):
-    def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, -1, title)
+    def __init__(self, parent, id):
+        wx.Frame.__init__(self, parent, -1, 'LDAP Browser')
 
         self.splitter = wx.SplitterWindow(self, ID_SPLITTER, style=wx.SP_BORDER)
         self.splitter.SetMinimumPaneSize(50)
@@ -48,9 +103,9 @@ class LdapBrowser(wx.Frame):
         self.Close(True)
 
     def ConnectMenu(self,e):
-        pass
+        chgdep = ConnectWindow(self,-1)
 
 app = wx.App(0)
-LdapBrowser(None, -1, 'LDAP Browser')
+LdapBrowser(None, -1)
 app.MainLoop()
 
