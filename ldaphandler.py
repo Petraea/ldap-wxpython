62d3f046c1cb35370c28157593334d8f3582a71e
Petraea <g.j.s.dawes@gmail.com>
2016-12-11 22:49:35 +0100

    Expand template.

--- ldaphandler.py
+++ ldaphandler.py
@@ -1,6 +1,6 @@
 import logging, sys
 import ldap
-import ConfigParser
+from confighandler import Config
 
 #Temporary testing code
 root = logging.getLogger()
@@ -11,23 +11,6 @@ ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 root.addHandler(ch)
-
-class Config:
-    def __init__(self,path='default.conf'):
-        self.ldapuri='ldap://'
-        self.binddn=''
-        self.password=None
-        self.sasl=False
-        self.force2=False
-        self.force3=False
-        self.path=path
-        config = ConfigParser.ConfigParser()
-        config.read(path)
-        for f in self.__dict__.keys():
-            try:
-                setattr(self,f,config.get('config',f))
-            except:
-                logging.debug('No field %s defined in config.' % f)
 
 class LDAP:
     def __init__(self,config=None):
@@ -41,6 +24,65 @@ class LDAP:
         rootdse = self.ldap.search_s('',ldap.SCOPE_BASE,'(objectclass=*)',['namingContexts'])
         self.basedn = rootdse[0][1]['namingContexts'][0]
 
-#Attempt to find base dc with nothing
+    def getChildren(self,dn):
+        '''Wrapper function for treebrowsing. Just get me the DNs that are under this one.'''
+        res = [x[0] for x in self.ldap.search_s(dn,ldap.SCOPE_ONELEVEL,attrlist=['dn'])]
+        return res
+
+    def getAttrs(self,dn):
+        '''Wrapper function for objectviewing.'''
+        res = self.ldap.search_s(dn,ldap.SCOPE_BASE)
+        return res[0][1]
+
+    def replaceAttrs(self,dn,attrs):
+        '''Wrapper function for modifications. Will entirely replace attributes, removing non-existent ones.'''
+        curattrs=self.getAttrs(dn).keys()
+        newattrs={x:attrs[x] for x in attrs if x not in curattrs}
+        modattrs={x:attrs[x] for x in attrs if x in curattrs}
+        delattrs={x:attrs[x] for x in curattrs if x not in attrs}
+        #do modify_s functions
+        return None
+
+    def modifyAttrs(self,dn,attrs):
+        '''Wrapper function for modifications. Will add or modify existing attrs, but not delete.'''
+        curattrs=self.getAttrs(dn).keys()
+        newattrs={x:attrs[x] for x in attrs if x not in curattrs}
+        modattrs={x:attrs[x] for x in attrs if x in curattrs}
+        #do modify_s functions
+        return None
+
+    def deleteAttrs(self,dn,attrs):
+        '''Wrapper function for modifications. Will only delete attributes.'''
+        curattrs=self.getAttrs(dn).keys()
+        delattrs={x:attrs[x] for x in curattrs if x not in attrs}
+        #do modify_s functions
+        return None
+
+    def deleteObject(self,dn):
+        '''Wrapper function for modifications. Delete entire object.'''
+        #do delete_s functions
+        return None
+
+    def createObject(self,dn,attrs={}):
+        '''Wrapper function for modifications. Adds entire object, with optional attributes set.'''
+        #do add_s functions
+        return None
+
+    def copyObject(self,olddn,newdn):
+        '''Wrapper function for modifications. Copies entire object.'''
+        self.createObject(newdn,self.getAttrs(olddn))
+        return None
+
+    def moveObject(self,olddn,newdn):
+        '''Wrapper function for modifications. Moved an object.'''
+        #do rename_s functions
+        return None
+
+
 l = LDAP()
-print (l.ldap.search_s(l.basedn,ldap.SCOPE_SUBTREE,'(objectClass=*)'))
+print(l.getChildren(l.basedn))
+a = l.getChildren(l.basedn)[1]
+print(l.getChildren(a))
+b = l.getChildren(a)[2]
+print(l.getAttrs(b))
+
