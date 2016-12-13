0b7e37e4f795b34495f009750b82780a833af196
Gwen <petraea@nurdspace.nl>
2016-12-13 16:11:20 +0100

    Add read/write ability.

--- confighandler.py
+++ confighandler.py
@@ -5,14 +5,16 @@ class Config:
     def __init__(self,path='default.conf'):
         self.ldapuri='ldap://'
         self.binddn=''
-        self.password=None
-        self.sasl=False
-        self.force2=False
-        self.force3=False
+        self.password=''
+        self.sasl='false'
+        self.proto='auto'
         self.path=path
+        self.load()
+
+    def load(self):
         config = ConfigParser.ConfigParser()
         try:
-            config.read(path)
+            config.read(self.path)
             for f in self.__dict__.keys():
                 try:
                     setattr(self,f,config.get('config',f))
@@ -20,3 +22,14 @@ class Config:
                     logging.debug('No field %s defined in config.' % f)
         except:
             logging.debug('No config %s defined.' % path)
+
+    def save(self):
+        config = ConfigParser.ConfigParser()
+        config.add_section('config')
+        for f in self.__dict__.keys():
+            config.set('config',f,getattr(self,f))
+        try:
+            config.write(self.path)
+        except:
+            logging.error('Cannot save config to %s' % path)
+
